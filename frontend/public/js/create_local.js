f419afb2f1f4209545bf8078ae41a2a172f57d86
Charlzee <92670599+Charlzee@users.noreply.github.com>
2026-04-22 00:23:18 +0100

    update frontend/public/js/create_local.js

--- frontend/public/js/create_local.js
+++ frontend/public/js/create_local.js
@@ -2,7 +2,7 @@ const topic_container = document.getElementById("topics")
 
 console.log(topic_container)
 
-const response = await fetch("https://imposter-game-backend.charlierw1028.workers.dev")
+const response = await fetch("https://imposter-game-backend.charlierw1028.workers.dev/words")
 const data = await response.json()
 
 console.log(data)
