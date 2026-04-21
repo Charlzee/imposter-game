1f8d7279e9846f739a381aa87d074ac074f4dbe7
Charlzee <92670599+Charlzee@users.noreply.github.com>
2026-04-22 00:15:53 +0100

    update backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import words from 'words.json'
 
 const app = new Hono()
 app.use("*", cors())
@@ -7,3 +8,5 @@ app.use("*", cors())
 app.get("/words", (c) => {
     return c.json()
 })
+
+export default app
