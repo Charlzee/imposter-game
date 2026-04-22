e6ec0338b53343a8b1433a7b8963f177f602ee5b
Charlzee0 <charlierw1028@gmail.com>
2026-04-22 15:44:13 +0100

    refactor: update words.json structure and enhance create_local.js for topic display

--- frontend/public/js/create_local.js
+++ frontend/public/js/create_local.js
@@ -1,8 +1,39 @@
-const topic_container = document.getElementById("topics")
+const topic_container = document.getElementById("topic-container");
 
-console.log(topic_container)
+const response = await fetch("https://imposter-game-backend.charlierw1028.workers.dev/words");
+const data = await response.json();
 
-const response = await fetch("https://imposter-game-backend.charlierw1028.workers.dev/words")
-const data = await response.json()
+if (Array.isArray(data)) {
+    for (const topic of data) {
+        //Element
+        const topic_element = document.createElement("div");
+        topic_element.classList.add("topic");
 
-console.log(data)
+        //Title
+        const topic_title = document.createElement("h2");
+        topic_title.textContent = topic.display_name; 
+        
+        //Stats
+        const stats_container = document.createElement("div");
+        stats_container.classList.add("topic-stats");
+
+        //Difficulty
+        const difficulty = document.createElement("span");
+        difficulty.textContent = `Difficulty: ${topic.difficulty_imposter}`;
+        difficulty.style.fontSize = "0.8rem";
+
+        //Word Count
+        const word_count = document.createElement("span");
+        word_count.textContent = `Word Count: ${topic.words.length}`;
+        word_count.style.fontSize = "0.8rem";
+
+        //Append
+        topic_element.appendChild(topic_title);
+        topic_element.appendChild(stats_container);
+        
+        stats_container.appendChild(difficulty);
+        stats_container.appendChild(word_count);
+
+        topic_container.appendChild(topic_element);
+    }
+}
