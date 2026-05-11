5040299f93f9b690e3199508d8d660b5d7ae5b50
Charlzee <92670599+Charlzee@users.noreply.github.com>
2026-05-11 12:19:10 +0100

    Update play.js

--- frontend/public/js/play.js
+++ frontend/public/js/play.js
@@ -199,7 +199,7 @@ function displayRole(playerIndex) {
             const roleColorMatch = configUi.grad.match(/rgb\(.*?\)/);
             const roleColor = roleColorMatch ? roleColorMatch[0] : 'rgb(0, 255, 0)';
             roleDisplay.style.backgroundImage = `linear-gradient(to right, rgb(0, 0, 0) 0%, ${roleColor} 100%)`;
-            roleTip.textContent = `${configUi.tip}\n\nMODIFIER: ${ROLE_DATA.mimes.tip}`;
+            roleTip.textContent = `${configUi.tip}\n\n${ROLE_DATA.mimes.tip}`;
         } else {
             roleDisplay.style.backgroundImage = configUi.grad;
             roleTip.textContent = configUi.tip;
