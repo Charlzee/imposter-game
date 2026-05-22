6c83db2133269d423c64cdb2c81bb8f594807d50
Charlzee <92670599+Charlzee@users.noreply.github.com>
2026-05-22 09:54:56 +0100

    Fix gradient image path for monkey class

--- frontend/public/js/play.js
+++ frontend/public/js/play.js
@@ -101,7 +101,7 @@ const ROLE_MODIFIERS = {
         label: 'Monkey', class: 'monkey',
         tip: 'Your word/sentence must contain the letter/number: ',
         //grad: 'radial-gradient(circle, rgb(77, 43, 33) 0%, rgb(59, 19, 7) 100%)',
-        grad: '../assets/IMG_0494.jpeg',
+        grad: '/assets/IMG_0494.jpeg',
         textColor: 'rgb(151,101,48)',
         subTextColor: 'brown',
         showWord: true,
